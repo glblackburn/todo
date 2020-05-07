4cd25d4964cc76a75cf60199c23cdb9309704717
Lee Blackburn <g.lee.blackburn@gmail.com>
2020-05-07 16:06:14 -0400

    add showAbout. clean up messaging

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,44 +1,64 @@
-const productName = "A better TODO!"
+const productName = "A Better TODO!"
 // trying a fancy version following this example.
 // https://www.sitepoint.com/javascript-command-line-interface-cli-node-js/
 // the first version is in index_old.js
 
+const npm_libraries = [];
 const chalk = require('chalk');
+npm_libraries.push({name: 'chalk'})
 const clear = require('clear');
+npm_libraries.push({name: 'clear'})
 const figlet = require('figlet');
+npm_libraries.push({name: 'figlet', note: 'figlet font options: http://patorjk.com/software/taag/#p=testall&h=0&v=0&f=Rectangles&t=A%20Better%20TODO!'})
+
 const todo_dao = require('./lib/todo-dao')
 const inquirer  = require('./lib/inquirer');
+
 
 function banner() {
     clear();
     console.log(
 	chalk.green(
-	    figlet.textSync(productName, { horizontalLayout: 'full' })
+	    figlet.textSync(productName, {
+		font: 'Big Money-se',
+		horizontalLayout: 'full' })
 	)
     );
 }
 
+// TODO: complete the about function and add to menu options.
+function showAbout() {
+    console.log('node.js npm libraries')
+    npm_libraries.forEach(lib => {
+	console.log(`  ${lib.name}`)
+	console.log(`    ${lib.note}`)
+    })
+    
+    console.log('figlet font options: http://patorjk.com/software/taag/#p=testall&h=0&v=0&f=Rectangles&t=A%20Better%20TODO!')
+}
+
 function showTodos() {
+    console.log('List the TODOs')
     todo_dao.showTodoList()
 }
 
 function addTodo(todo) {
-    console.log('Adding TODO:')
+    console.log('Create a TODO')
     console.log(JSON.stringify(todo, null, '  '));
     todo_dao.add(todo.text);
-    console.log('Added TODO:')
+    console.log('TODO Created')
 }
 
 // TODO: would love to have a spinner added here.
 // https://github.com/nathanpeck/clui#spinnerstatustext
 function deleteTodos(todos) {
-    console.log('Deleting TODOs:')
+    console.log('Deleting TODOs')
     console.log(JSON.stringify(todos, null, '  '));
     todos.deletes.forEach(todo => {
 	//console.log(todo)
 	todo_dao.deleteById(todo)
     })
-    console.log('Deleted TODOs:')
+    console.log('TODOs Deleted')
 }
 
 const run = async () => {
@@ -65,7 +85,7 @@ const run = async () => {
 	    console.log('Exiting');
             break;
 	default:
-            console.log(`Unknown option: '${mainMenu.choice}'`);
+            console.log(`TODO: '${mainMenu.choice}' has not been implemented.`);
             break;
 	}
     }
