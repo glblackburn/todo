8262709da8c3ce27135e3b5c2b754b84243d2ed0
Lee Blackburn <g.lee.blackburn@gmail.com>
2020-05-07 05:45:45 -0400

    working on second level read

--- nodejs/menu.js
+++ nodejs/menu.js
@@ -1,4 +1,7 @@
+'use strict'
+
 const sprintf = require('sprintf-js').sprintf
+const readline = require('readline');
 const todo_dao = require('./todo-dao')
 
 function loadTestData() {
@@ -38,24 +41,29 @@ function showTodoList() {
     }
 }
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Enter letter:> '
+});
+
+function enterToContinue() {
+    console.log('Enter to continue:>')
+}
+
 function processCommand() {
-    const readline = require('readline');
-
-    const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-	prompt: 'Enter letter:> '
-    });
-
     showMenu()
     rl.prompt()
 
     rl.on('line', (line) => {
+	var hideMenu = false
 	var selection=line.trim()
 	switch (selection) {
 	case 'l':
 	    console.log('l. List the TODOs')
             showTodoList()
+	    enterToContinue()
+	    hideMenu = true
             break;
 	case 'c':
 	    console.log('c. Create a TODO')
@@ -75,13 +83,16 @@ function processCommand() {
 	case 'q':
 	    rl.close()
             break;
+	case '':
+            break;
 	default:
-            console.log('');
             console.log(`Unknown option: '${selection}'`);
             break;
 	}
-	showMenu()
-	rl.prompt();
+	if (!hideMenu) {
+	    showMenu()
+	    rl.prompt();
+	}
     }).on('close', () => {
 	showThankYouMessage()
 	process.exit(0);
