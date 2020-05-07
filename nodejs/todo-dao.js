b5fee9ec3aa06039f28853eeeb2de79ec265e3e7
Lee Blackburn <g.lee.blackburn@gmail.com>
2020-05-07 05:46:30 -0400

    add showTodoList and loadTestData

--- nodejs/todo-dao.js
+++ nodejs/todo-dao.js
@@ -1,5 +1,5 @@
-// https://github.com/uuidjs/uuid
 const uuidv1 = require('uuid/v1')
+const sprintf = require('sprintf-js').sprintf
 
 var todoList = []
 
@@ -17,13 +17,13 @@ function add( todoText ) {
 
 function getById( id ) {
     var todo = todoList.find(a => a.id === id)
-    console.log(`getById: id=[${id}] todo=[${todo}]`)
+    //console.log(`getById: id=[${id}] todo=[${todo}]`)
     return todo
 }
 
 function deleteById( id ) {
     var todo = getById(id)
-    console.log(`removeById: id=[${id}] todo=[${todo}]`)
+    //console.log(`removeById: id=[${id}] todo=[${todo}]`)
     var newTodoList = todoList.filter(function(value, index, arr){ return value.id != id;})
     todoList = newTodoList
     return todo
@@ -33,7 +33,23 @@ function getTodoList() {
     return todoList
 }
 
+function showTodoList() {
+    var format = '%-40s | %-20s'
+    console.log(sprintf(format, 'ID','Text'))
+    for (var i = 0 ; i < todoList.length ; i++) {
+	console.log(sprintf(format, todoList[i].id, todoList[i].text))
+    }
+}
+
+function loadTestData() {
+    add("test data 1")
+    add("test data 2")
+    add("test data 3")
+}
+
 exports.add = add;
 exports.get = getById;
 exports.deleteById = deleteById;
 exports.getTodoList = getTodoList;
+exports.showTodoList = showTodoList;
+exports.loadTestData = loadTestData;
